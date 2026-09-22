b0ddabf9394cedb914ba909a0ba3990c522b9ba7
Oksana Bohuslavska <oksanabohuslavska@Oksanas-MacBook-Air.local>
2026-09-22 13:33:04 -0700

    Complete Waves 1 and 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,22 @@
 class Vendor:
-    pass
+    def __init__(self, inventory=None):
+        if inventory is None:
+            inventory = []
+
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+    
+    def remove(self,item):
+        if item in self.inventory:
+            self.inventory.remove(item)
+            return item
+        return None
+    
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
