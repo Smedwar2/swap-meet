dc09293f09671d1a6f2c6f8f1dcce7102eda13e4
Shameka <Shamekam.edwards@gmail.com>
2026-09-22 17:34:54 -0500

    Complete Wave 3 w/ test

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,3 +1,4 @@
+##Wave 2##
 import uuid
 
 class Item:
@@ -10,5 +11,11 @@ class Item:
     def get_category(self):
         return "Item"
 
-    
+###Wave 3###
+# 1.Stringify instance of Item class
+# 2. this method returns a string representation of the Item instance.
+# 3. Returns a string in the format: "An object of type <category> with id <id>."
+    def __str__(self):
+        # return the required sentence
+        return f"An object of type {self.get_category()} with id {self.id}."
         
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -14,9 +14,23 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return None
-    
+
+    ##Wave 2##
     def get_by_id(self, id):
         for item in self.inventory:
             if item.id == id:
                 return item
         return None
+
+    ###wave 3###
+    #1. Swap my_item with their_item between self and other_vendor and return true
+    #2. False if either inventory is missing the respective item.
+    #3. Remove my_item from self and add their_item, and vice versa for other_vendor.
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item not in self.inventory or their_item not in other_vendor.inventory:
+            return False
+        self.remove(my_item)
+        other_vendor.add(my_item)
+        other_vendor.remove(their_item)
+        self.add(their_item)
+        return True
